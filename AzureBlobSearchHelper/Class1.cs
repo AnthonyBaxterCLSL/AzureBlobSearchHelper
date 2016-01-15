104c731eaf040dfdd3c8815c0bb751b1fdad0850
Anthony Baxter <anthony.baxter@clsl.co.uk>
2016-01-15 21:47:42 +0000

    Initial commit

---
+++ AzureBlobSearchHelper/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AzureBlobSearchHelper
+{
+    public class Class1
+    {
+        public string foo()
+        {
+            return "bar";
+        }
+    }
+}
