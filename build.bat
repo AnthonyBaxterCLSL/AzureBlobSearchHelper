a118ea6eebc04c2d692898462c47c6865cfba2b8
Anthony Baxter <anthony.baxter@clsl.co.uk>
2016-01-16 20:57:02 +0000

    update build.bat

--- build.bat
+++ build.bat
@@ -9,19 +9,19 @@ if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
 )
 
-set nunit="tools\nunit\nunit-console.exe"
+set nunit="packages\NUnit.Console.3.0.1\tools\nunit3-console.exe"
 
 REM Build
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild YourSolution.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild AzureBlobSearchHelper.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
-%nunit% YourSolution\ProjectA.Tests\bin\%config%\ProjectA.Tests.dll
+%nunit% AzureBlobSearchHelper\AzureBlobSearchHelper.Tests\bin\%config%\AzureBlobSearchHelper.Tests.dll
 if not "%errorlevel%"=="0" goto failure
 
 REM Package
 mkdir Build
-call %nuget% pack "YourSolution\YourSolution.ProjectA.csproj" -symbols -o Build -p Configuration=%config% %version%
+call %nuget% pack "AzureBlobSearchHelper\AzureBlobSearchHelper\AzureBlobSearchHelper.csproj" -symbols -o Build -p Configuration=%config% %version%
 if not "%errorlevel%"=="0" goto failure
 
 :success
